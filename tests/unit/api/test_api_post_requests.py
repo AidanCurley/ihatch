96e60df7dd6911d9ad9f1c0613708c85b7be7029
AidanCurley <aidanpcurley@gmail.com>
2022-07-10 10:49:29 +0100

    DEV-33 Add more tests for POST endpoints

--- tests/unit/api/test_api_post_requests.py
+++ tests/unit/api/test_api_post_requests.py
@@ -23,16 +23,16 @@ class APIPostRequestsCase(unittest.TestCase):
             '/wrong/url')
         self.assertEqual(response.status_code, 404)
 
-    def test_create_user_with_valid_parameters(self):
+    def test_create_user_with_valid_parameters_returns_status_ok(self):
         with self.app.app_context():
             user = User(name='john', surname='smith', email='john@example.com', hash='123456', dob=date(2005, 6, 1))
 
         response = self.client.post('/create_user', json={
-            "name": "Billy",
-            "surname": "NoMates",
-            "dob": "1980-01-01",
-            "email": "billy@friendless.com",
-            "hash": "nomates"
+            "name": "john",
+            "surname": "smith",
+            "dob": "2005-06-01",
+            "email": "john@example.com",
+            "hash": "123456"
         })
 
         self.assertEqual(response.status_code, 200)
@@ -40,16 +40,16 @@ class APIPostRequestsCase(unittest.TestCase):
 
         # Check that user has been inserted in the database
         with self.app.app_context():
-            self.assertEqual(db.session.query(User).filter(User.email == "billy@friendless.com").count(), 1)
+            self.assertEqual(db.session.query(User).filter(User.email == user.email).count(), 1)
 
-    def test_create_hatch_with_valid_parameters(self):
+    def test_create_hatch_with_valid_parameters_returns_status_ok(self):
         with self.app.app_context():
             hatch = Hatch(user_id=1, sensor_id=1, is_active=True, start_date=date(2022, 1, 1))
 
         response = self.client.post('/create_hatch', json={
             'is_active': True,
             'sensor_id': 1,
-            'start_date': 'Sat, 01 Jan 2022 00:00:00 GMT',
+            'start_date': '2022-01-01',
             'user_id': 1
         })
 
@@ -58,30 +58,30 @@ class APIPostRequestsCase(unittest.TestCase):
 
         # Check that hatch has been inserted in the database
         with self.app.app_context():
-            self.assertEqual(db.session.query(Hatch).filter(Hatch.start_date == 'Sat, 01 Jan 2022 00:00:00 GMT').count(), 1)
+            self.assertEqual(db.session.query(Hatch).filter(Hatch.start_date == hatch.start_date).count(), 1)
 
-    def test_create_egg_with_valid_parameters(self):
+    def test_create_egg_with_valid_parameters_returns_status_ok(self):
         with self.app.app_context():
-            egg = Egg(hatch_id=1, start_date=date(2022, 6, 1), end_date=None, hatched=False)
+            egg = Egg(hatch_id=1, start_date=date(2022, 1, 1), end_date=None, hatched=False)
 
         response = self.client.post('/create_egg', json={'end_date': None,
                                                          'hatch_id': 1,
                                                          'hatched': False,
                                                          'id': 1,
-                                                         'start_date': 'Wed, 01 Jun 2022 00:00:00 GMT'})
+                                                         'start_date': '2022-01-01'})
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {'Status': 'OK'})
 
         # Check that egg has been inserted in the database
         with self.app.app_context():
-            self.assertEqual(db.session.query(Egg).filter(Egg.start_date == 'Wed, 01 Jun 2022 00:00:00 GMT').count(), 1)
+            self.assertEqual(db.session.query(Egg).filter(Egg.start_date == egg.start_date).count(), 1)
 
-    def test_log_weight_with_valid_parameters(self):
+    def test_log_weight_with_valid_parameters_returns_status_ok(self):
         with self.app.app_context():
-            weight = Weight(egg_id='john', date_time=datetime(2022, 6, 1), weight=12.2)
+            weight = Weight(egg_id='john', date_time=date(2022, 6, 1), weight=12.2)
 
-        response = self.client.post('/log_weight', json={'date_time': 'Wed, 01 Jun 2022 00:00:00 GMT',
+        response = self.client.post('/log_weight', json={'date_time': '2022-06-01',
                                                          'egg_id': 'john',
                                                          'id': 1,
                                                          'weight': 12.2})
@@ -91,17 +91,17 @@ class APIPostRequestsCase(unittest.TestCase):
 
         # Check that weight has been inserted in the database
         with self.app.app_context():
-            self.assertEqual(db.session.query(Weight).filter(Weight.date_time == 'Wed, 01 Jun 2022 00:00:00 GMT').count(), 1)
+            self.assertEqual(db.session.query(Weight).filter(Weight.date_time == weight.date_time).count(), 1)
 
-    def test_log_measurement_with_valid_parameters(self):
+    def test_log_measurement_with_valid_parameters_returns_status_ok(self):
         with self.app.app_context():
-            measurement = Measurement(sensor_id=1, date_time=datetime(2022, 6, 1), temperature=12.2, humidity=40.25)
+            measurement = Measurement(sensor_id=1, date_time=date(2022, 6, 1), temperature=12.2, humidity=40.25)
 
         response = self.client.post('/log_measurement', json={
-            "sensor_id": 3,
-            "date_time": "2022-07-07T15:09:30",
-            "temperature": 41.3,
-            "humidity": 40
+            "sensor_id": 1,
+            "date_time": "2022-06-01",
+            "temperature": 12.2,
+            "humidity": 40.25
         })
 
         self.assertEqual(response.status_code, 200)
@@ -109,4 +109,72 @@ class APIPostRequestsCase(unittest.TestCase):
 
         # Check that measurement has been inserted in the database
         with self.app.app_context():
-            self.assertEqual(db.session.query(Measurement).filter(Measurement.date_time == '2022-07-07T15:09:30').count(), 1)
+            self.assertEqual(db.session.query(Measurement).filter(Measurement.date_time == measurement.date_time).count(), 1)
+
+    def test_create_user_with_missing_parameter_returns_error(self):
+        response = self.client.post('/create_user', json={
+            "name": "Billy",
+            "surname": "NoMates",
+            "dob": "1980-01-01",
+            "hash": "nomates"
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'Error': {
+            "name": "Billy",
+            "surname": "NoMates",
+            "dob": "1980-01-01",
+            "hash": "nomates"
+        }})
+
+
+    def test_create_hatch_with_missing_parameter_returns_error(self):
+        response = self.client.post('/create_hatch', json={
+            'is_active': True,
+            'sensor_id': 1,
+            'user_id': 1
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'Error': {
+            'is_active': True,
+            'sensor_id': 1,
+            'user_id': 1
+        }})
+
+    def test_create_egg_with_missing_parameter_returns_error(self):
+        response = self.client.post('/create_egg', json={'end_date': None,
+                                                         'hatch_id': 1,
+                                                         'hatched': False})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'Error': {'end_date': None,
+                                                         'hatch_id': 1,
+                                                         'hatched': False}})
+
+    def test_log_weight_with_missing_parameter_returns_error(self):
+        response = self.client.post('/log_weight', json={'egg_id': '1',
+                                                         'id': 1,
+                                                         'weight': 12.2})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'Error': {'egg_id': '1',
+                                                         'id': 1,
+                                                         'weight': 12.2}})
+
+    def test_log_measurement_with_missing_parameter_returns_error(self):
+        with self.app.app_context():
+            measurement = Measurement(sensor_id=1, date_time=datetime(2022, 6, 1), temperature=12.2, humidity=40.25)
+
+        response = self.client.post('/log_measurement', json={
+            "sensor_id": 3,
+            "temperature": 41.3,
+            "humidity": 40
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'Error': {
+            "sensor_id": 3,
+            "temperature": 41.3,
+            "humidity": 40
+        }})
